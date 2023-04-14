ca40053c8612aab46adcb3b08f4263dd850fd764
Vasya207 <khymynetsvasya@gmail.com>
2023-04-14 09:28:06 +0300

    Added particle effect

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -6,23 +6,29 @@ public class Circle : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     private new Rigidbody2D rigidbody;
-    public float circleDiameter;
+    private new ParticleSystem particleSystem;
+    private ParticleSystem.MainModule settings;
     
     private PointsManager pointsManager;
     private CircleSpawner circleSpawner;
+    
+    public float circleDiameter;
 
     private void Awake()
     {
+        particleSystem = FindObjectOfType<ParticleSystem>();
         pointsManager = FindObjectOfType<PointsManager>();
         circleSpawner = FindObjectOfType<CircleSpawner>();
         rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        settings = FindObjectOfType<ParticleSystem>().main;
     }
 
     private void OnMouseDown()
     {
         circleSpawner.DeactivateCircle(this);
         pointsManager.AddPoints(circleDiameter);
+        PlayParticles();
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -32,7 +38,14 @@ public class Circle : MonoBehaviour
             circleSpawner.DeactivateCircle(this);
         }
     }
-    
+
+    private void PlayParticles()
+    {
+        particleSystem.transform.position = transform.position;
+        settings.startColor = new ParticleSystem.MinMaxGradient(spriteRenderer.color);
+        particleSystem.Play();
+    }
+
     public void SetUpSpeed(Vector2 vel)
     {
         rigidbody.velocity = vel;
