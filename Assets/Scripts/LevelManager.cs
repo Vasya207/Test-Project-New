d94638890375f319e76d4b425c38a30dff54bde0
Vasya207 <khymynetsvasya@gmail.com>
2023-05-24 17:54:09 +0300

    Removed AssetBundles

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -27,6 +27,5 @@ public class LevelManager : Singleton<LevelManager>
         currentLevel++;
         Signals.OnNewLevel.Invoke(currentLevel);
         startingPointsBarrier *= 2;
-        setUpBackground.ChangeBackground();
     }
 }
